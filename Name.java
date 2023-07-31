06bc72d67c2ca4ceb93a39a823e9fe91062d1e4c
Your Name <email@example.com>
2023-07-31 13:40:01 -0700

    first commit

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Name {
+    public static void main(String[] args) {
+        String name = "Sylvia Dang";
+        int age = 23;
+        String hometown = "San Leandro, Ca";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
